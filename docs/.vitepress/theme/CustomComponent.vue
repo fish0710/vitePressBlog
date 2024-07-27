24e9f14f45aaab2485cb7796ca11c56230aeb6a2
钟琰琰 <11196663+zhongyanyan59@user.noreply.gitee.com>
2024-07-27 13:06:44 +0800

    test

--- docs/.vitepress/theme/CustomComponent.vue
+++ docs/.vitepress/theme/CustomComponent.vue
@@ -2,12 +2,4 @@
 </template>
 
 <script setup>
-import { loadOml2d }  from 'oh-my-live2d';
-loadOml2d({
-    models: [
-        {
-        path: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/hijiki/hijiki.model.json'
-        }
-    ]
-});
 </script>
