e96ca4a524c62b90dcb912a6543ded144c2ae24f
fish59 <1595523361@qq.com>
2024-07-27 16:04:19 +0800

    321321321

--- docs/.vitepress/theme/CustomComponent.vue
+++ docs/.vitepress/theme/CustomComponent.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>123123</div>
+    <div></div>
 </template>
 
 <script setup>
-   
+    import { loadOml2d } from 'oh-my-live2d';
+    loadOml2d({
+        models: [
+          {
+            path: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/hijiki/hijiki.model.json'
+          }
+        ]
+      });
 </script>
