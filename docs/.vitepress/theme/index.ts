1ce44ec0e9ea687fd58a25cacc1d87885918b7f9
fish59 <1595523361@qq.com>
2024-07-28 00:02:27 +0800

    fix: 修复看板娘进入首页重新加载

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,13 +2,24 @@
 import type { Theme } from 'vitepress'
 import CustomComponent from './CustomComponent.vue'
 import DefaultTheme from 'vitepress/theme'
-import { Button } from 'element-ui';
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // 注册自定义全局组件
-    app.component('CustomComponentfrom',CustomComponent /* ... */)
+  async enhanceApp({ app }) {
+    const { loadOml2d } = await import('oh-my-live2d');
 
+    // 使用 mixin 在所有组件中添加 mounted 钩子
+    
+        loadOml2d({
+          models: [
+            {
+              path: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/hijiki/hijiki.model.json'
+            }
+          ]
+        });
+
+
+    // 注册自定义全局组件
+    app.component('CustomComponentfrom', CustomComponent /* ... */)
   }
 } satisfies Theme
