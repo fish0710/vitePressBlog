71c483059f3d1180307c75899338d31081ef9054
fish59 <1595523361@qq.com>
2024-07-28 14:17:06 +0800

    feat: 解决live2d组件冗余

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 // .vitepress/theme/index.ts
 import type { Theme } from 'vitepress'
-import CustomComponent from './CustomComponent.vue'
+import CustomComponent from './components/CustomComponent.vue'
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   async enhanceApp({ app }) {
     app.config.globalProperties.isLoadLive2d = false;//全局设置一个属性是否加载live2d，避免重复加载
     app.component('CustomComponentfrom', CustomComponent /* ... */)
